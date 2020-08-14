2ce7980b848191ea563f5af8ab6a09fd53a9dd28
Aramis Razzaghipour <aramisnoah@gmail.com>
2020-08-15 00:08:17 +1000

    Highlight semantic token strings

--- src/main.rs
+++ src/main.rs
@@ -122,6 +122,8 @@ impl fmt::Display for Theme {
             writeln!(f, "{},", self.comments)?;
         }
 
+        writeln!(f, r#""string": {},"#, self.strings)?;
+
         write!(f, r#""keyword": "#)?;
         if self.are_keywords_bold {
             writeln!(f, r#"{{"bold":true,"foreground":{},}},"#, self.keywords)?;
