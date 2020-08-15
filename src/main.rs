0f432fedab7da5fbdaedd7ed32ca77cb3baa4453
Aramis Razzaghipour <aramisnoah@gmail.com>
2020-08-15 20:07:13 +1000

    Give conflicting Git resources the same colour as errors

--- src/main.rs
+++ src/main.rs
@@ -358,7 +358,7 @@ impl fmt::Display for Theme {
 
         write_scope(f, "gitDecoration.untrackedResourceForeground", DARK_FG)?;
         write_scope(f, "gitDecoration.addedResourceForeground", DARK_FG)?;
-        write_scope(f, "gitDecoration.conflictingResourceForeground", DARK_FG)?;
+        write_scope(f, "gitDecoration.conflictingResourceForeground", DARK_ERROR)?;
         write_scope(f, "gitDecoration.deletedResourceForeground", DARK_FG)?;
         write_scope(f, "gitDecoration.ignoredResourceForeground", DARK_FG)?;
         write_scope(f, "gitDecoration.modifiedResourceForeground", DARK_FG)?;
